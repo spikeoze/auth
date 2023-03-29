d372b85ea2648fbd4d311e553365bd56881c0049
Mukhtar Amin <mukhtaaramiin@gmail.com>
2023-03-29 09:16:02 +0300

    changed cors origin

--- client/src/api/apifunctions.ts
+++ client/src/api/apifunctions.ts
@@ -3,7 +3,7 @@ import { CurrentUser, Post } from "../interfaces/interfaces";
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: process.env.NEXT_PUBLIC_API_ROUTE,
   withCredentials: true,
 });
 
